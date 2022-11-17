f933107faca65b6963bc4cdfdead19aee357d166
Konovalenko Pavel <konovalenkopavel@gmail.com>
2022-11-17 07:29:39 +0300

    create toggle view buttons

---
+++ src/pages/Contacts/ToggleDataViewMode/ToggleDataViewMode.js
@@ -0,0 +1,42 @@
+import { useCallback } from "react";
+import PropTypes from "prop-types";
+
+const ToggleDataViewMode = ({ setDataViewMode, dataViewMode }) => {
+  const handleChangeDataViewMode = useCallback(
+    (dataView) => {
+      setDataViewMode(dataView);
+      localStorage.setItem("dataViewMode", dataView);
+    },
+    [setDataViewMode]
+  );
+  return (
+    <div className="btn-group">
+      <a
+        href="#"
+        className={
+          "btn btn-primary" + (dataViewMode === "table" ? " active" : "")
+        }
+        aria-current="page"
+        onClick={() => handleChangeDataViewMode("table")}
+      >
+        <i className="bi bi-table"></i>
+      </a>
+      <a
+        href="#"
+        className={
+          "btn btn-primary" + (dataViewMode === "table" ? "" : " active")
+        }
+        onClick={() => handleChangeDataViewMode("grid")}
+      >
+        <i className="bi bi-grid"></i>
+      </a>
+    </div>
+  );
+};
+
+export default ToggleDataViewMode;
+
+ToggleDataViewMode.propTypes = {
+  setDataViewMode: PropTypes.func.isRequired,
+  dataViewMode: PropTypes.oneOf(["table", "grid"]).isRequired,
+};
--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,49 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ContactsTable } from "./ContactsTable";
-
-const useContacts = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  useEffect(() => {
-    const getContacts = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch("https://randomuser.me/api/?results=200");
-        const { results, error } = await response.json();
-        if (error) {
-          throw new Error(error);
-        }
-        setData(results);
-        setIsError(false);
-      } catch (e) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getContacts();
-  }, []);
-  return {
-    data,
-    isLoading,
-    isError,
-  };
-};
+import ToggleDataViewMode from "./ToggleDataViewMode/ToggleDataViewMode";
+import { useContacts } from "./useContacts";
 
 export const Contacts = () => {
   const contacts = useContacts();
+  const [dataViewMode, setDataViewMode] = useState(
+    localStorage.getItem("dataViewMode") || "table"
+  );
 
-  if (contacts.isLoading) {
-    return <div>loading...</div>;
-  }
-  if (contacts.isError) {
-    return <div>...error</div>;
-  }
   return (
     <div className="container">
-      <h1>Contacts</h1>
-      <ContactsTable contacts={contacts.data} />
+      <div className="d-flex justify-content-between">
+        <h1>Contacts</h1>
+        <ToggleDataViewMode
+          setDataViewMode={setDataViewMode}
+          dataViewMode={dataViewMode}
+        />
+      </div>
+      {() => {
+        if (contacts.isLoading) {
+          return <div>loading...</div>;
+        }
+        if (contacts.isError) {
+          return <div>...error</div>;
+        }
+        if (dataViewMode === "table") {
+          return <ContactsTable contacts={contacts.data} />;
+        }
+        if (dataViewMode === "grid") {
+          return <div>Grid</div>;
+        }
+      }}
     </div>
   );
 };
