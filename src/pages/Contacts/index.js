882647c4077c2b6cb4230e6609979a9ac97167e9
Konovalenko Pavel <konovalenkopavel@gmail.com>
2022-11-23 08:51:57 +0300

    add statistic of nationalities

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -5,6 +5,7 @@ import MainContent from "./MainContent";
 import Pagination from "./Pagination/Pagination";
 import ToggleDataViewMode from "./ToggleDataViewMode/ToggleDataViewMode";
 import { useContacts } from "./useContacts";
+import { Nationality } from "../../constantce/nationality";
 
 const FiltersDefaultValue = {
   fullname: "",
@@ -93,6 +94,25 @@ export const Contacts = () => {
   const whoPredominate =
     malesCount > famaleCount ? "male predominate" : "famale predominate";
 
+  const nationalitiesCount = () => {
+    const nationalitiesObj = {};
+    for (let i = 0; i < contactsAfterAZFilter.length; i++) {
+      const nat = contactsAfterAZFilter[i].nat;
+      if (nationalitiesObj[nat]) {
+        nationalitiesObj[nat] = nationalitiesObj[nat] + 1;
+      } else {
+        nationalitiesObj[nat] = 1;
+      }
+    }
+    return Object.entries(nationalitiesObj).map(([key, value]) => {
+      return (
+        <span key={key}>
+          {Nationality[key]}: {value}{" "}
+        </span>
+      );
+    });
+  };
+
   return (
     <div className="container">
       <div className="d-flex justify-content-between">
@@ -117,13 +137,13 @@ export const Contacts = () => {
       />
 
       <h3>Statistic</h3>
-      <div className="d-flex alert alert-primary">
+      <div className="d-flex justify-content-around alert alert-primary">
         <div className="d-flex flex-column">
           <span>Collection size</span>
           <div>{contactsAfterAZFilter.length}</div>
         </div>
         <div className="d-flex flex-column">
-          <div className="d-flex">
+          <div className="d-flex justify-content-around">
             <div className="d-flex flex-column">
               <span>Males</span>
               <div>{malesCount}</div>
@@ -136,6 +156,8 @@ export const Contacts = () => {
           <div>{whoPredominate}</div>
         </div>
       </div>
+      <h5>Nationalities</h5>
+      <div className="alert alert-secondary">{nationalitiesCount()}</div>
       <Pagination
         contacts={filtredContacts}
         setCurrentPage={setCurrentPage}
